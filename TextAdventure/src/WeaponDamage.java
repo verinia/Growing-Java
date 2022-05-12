14579853c50354e2b3e291ebb7c13aa1bb396638
cjw0621 <77221921+cjw0621@users.noreply.github.com>
2022-05-12 18:15:19 -0400

    added crit modifier

--- TextAdventure/src/WeaponDamage.java
+++ TextAdventure/src/WeaponDamage.java
@@ -1,7 +1,21 @@
 public class WeaponDamage {
+    static int crit(){
+        int crit = Root.randInt(1, 100);
+        if(crit >= 91){
+            return 10;
+        } return 0;
+    }
 
     public static void main(String[] args) {
+        int crit = crit();
+        int f_class = Root.randInt(0,(2 + crit));
 
+        int count = 0;
+        while(count<=100) {
 
+            System.out.println(crit());
+            System.out.println(f_class);
+            count+=1;
+        }
     }
 }
