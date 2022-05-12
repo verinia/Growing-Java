773b77c31b82b3bf07379319e02f8d20da27af76
cjw0621 <77221921+cjw0621@users.noreply.github.com>
2022-05-12 18:50:39 -0400

    Added Weapon Damage Class

--- TextAdventure/src/WeaponDamage.java
+++ TextAdventure/src/WeaponDamage.java
@@ -1,21 +1,63 @@
 public class WeaponDamage {
-    static int crit(){
-        int crit = Root.randInt(1, 100);
-        if(crit >= 91){
-            return 10;
-        } return 0;
+    private int crit;
+    private int f_Class;
+    private int c_Class;
+    private int b_Class;
+    private int a_Class;
+    private int s_Class;
+
+   public WeaponDamage(){
+       this.setCrit(crit);
+       this.setF_Class(f_Class);
+       this.setC_Class(c_Class);
+       this.setB_Class(b_Class);
+       this.setA_Class(a_Class);
+       this.setS_Class(s_Class);
+
+
+   }
+   protected void setCrit(int crit){
+       this.crit = crit;
+   }
+   public int getCrit(){
+       if(Root.randInt(1, 100) >= 92){
+           return 10;
+       } return 0;
+   }
+
+   protected void setF_Class(int f_class){
+       this.f_Class=f_class;
+   }
+   public int getF_Class(){
+       return this.f_Class = Root.randInt(0,2);
+   }
+
+   protected void setC_Class(int c_class){
+       this.c_Class = c_class;
+   }
+   public int getC_Class(){
+       return this.c_Class = Root.randInt((getCrit() / 2) + 2, 5 + (getCrit() - 4));
+   }
+
+    protected void setB_Class(int b_class){
+        this.b_Class = b_class;
+    }
+    public int getB_Class(){
+        return this.b_Class = Root.randInt((getCrit() / 2) + 4, 7 + (getCrit() - 3));
     }
 
-    public static void main(String[] args) {
-        int crit = crit();
-        int f_class = Root.randInt(0,(2 + crit));
-
-        int count = 0;
-        while(count<=100) {
-
-            System.out.println(crit());
-            System.out.println(f_class);
-            count+=1;
-        }
+    protected void setA_Class(int a_class){
+        this.a_Class = a_class;
     }
+    public int getA_Class(){
+        return this.a_Class = Root.randInt((getCrit() / 2) + 5, 9 + (getCrit() - 2));
+    }
+
+    protected void setS_Class(int s_class){
+        this.s_Class = s_class;
+    }
+    public int getS_Class(){
+        return this.s_Class = Root.randInt((getCrit() / 2) + 6, 12 + getCrit());
+    }
+
 }
