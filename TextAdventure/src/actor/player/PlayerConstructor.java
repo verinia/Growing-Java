32c4453795237f0f7e4475ee0e472e507e1e02fd
cjw0621 <77221921+cjw0621@users.noreply.github.com>
2022-05-16 22:36:42 -0400

    updated constructors in PlayerBuilder

--- TextAdventure/src/actor/player/PlayerConstructor.java
+++ TextAdventure/src/actor/player/PlayerConstructor.java
@@ -10,8 +10,6 @@ import lombok.Setter;
 
 
 public class PlayerConstructor {
-    //TODO: Create player object, constructor, create an AC modifier constructor that incorporates Armor, and shields,
-    // as well as certain spells. Also create player creation class that users can use to create a character.
 
     private int healthPoints;
     private int spellSlots;
