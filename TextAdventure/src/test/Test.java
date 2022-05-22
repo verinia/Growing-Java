81bb40eaa0b8f8097e671b9d1d5a4bfd12f78d70
cjw0621 <77221921+cjw0621@users.noreply.github.com>
2022-05-22 18:10:33 -0400

    added Player class constructors

--- TextAdventure/src/test/Test.java
+++ TextAdventure/src/test/Test.java
@@ -1,11 +1,37 @@
 package test;
 
 import actor.npc.impl.FriendlyNPC;
+import game_mech.DiceClass;
 
 public class Test {
 
     public static void main(String[] args) {
-        System.out.println(FriendlyNPC.ccGateGuard1.getArmorClass());
+
+        int guardHealth = FriendlyNPC.ccGateGuard1.getHealthPoints();
+
+        while(guardHealth > 0) {
+            int playerHit = (5 * (DiceClass.ROLL_DICE("d6"))) + 2;
+            if ((playerHit) >= FriendlyNPC.ccGateGuard1.getArmorClass()) {
+                System.out.println("You struck the Gate guard for " + playerHit + " damage");
+                guardHealth = guardHealth - playerHit;
+
+                if(guardHealth - playerHit <= 0) {
+                    guardHealth = 0;
+
+                }
+            } else {
+                System.out.println("You missed");
+            }
+
+            if (guardHealth == 0) {
+                System.out.println("The gate guard has been defeated");
+                System.out.println("Congratulations! You have gained 320 xp.");
+            }
+        }
+
+
+
+
     }
 
 }
