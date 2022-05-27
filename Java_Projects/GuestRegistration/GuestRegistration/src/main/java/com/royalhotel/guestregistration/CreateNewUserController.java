2df0e1fffe887f9e77238b06f4df6554de0a59d7
cjw0621 <77221921+cjw0621@users.noreply.github.com>
2022-05-27 13:08:42 -0400

    updated application

--- Java_Projects/GuestRegistration/GuestRegistration/src/main/java/com/royalhotel/guestregistration/CreateNewUserController.java
+++ Java_Projects/GuestRegistration/GuestRegistration/src/main/java/com/royalhotel/guestregistration/CreateNewUserController.java
@@ -4,15 +4,19 @@ import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
+import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.control.PasswordField;
 import javafx.scene.control.TextField;
 import javafx.stage.Stage;
 import javafx.stage.Window;
 
+import java.io.IOException;
+
 public class CreateNewUserController {
     public Label errorLabel;
     public Label successLabel;
+    public Button backButton;
     @FXML
     private TextField createUsernameField;
     @FXML
@@ -49,9 +53,10 @@ public class CreateNewUserController {
 
     }
     @FXML
-    public void onBackButton(ActionEvent actionEvent) {
+    public void onBackButton(ActionEvent actionEvent) throws IOException {
 
-
+        Stage stage = (Stage) backButton.getScene().getWindow();
+        stage.close();
 
 
     }
