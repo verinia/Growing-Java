8a3ef5d043cc4e17b350bba55d55e2de0d90ebdd
cjw0621 <77221921+cjw0621@users.noreply.github.com>
2022-05-27 00:01:49 -0400

    updated application

---
+++ Java_Projects/GuestRegistration/GuestRegistration/src/main/java/com/royalhotel/guestregistration/CreateNewUserController.java
@@ -0,0 +1,29 @@
+package com.royalhotel.guestregistration;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.PasswordField;
+import javafx.scene.control.TextField;
+
+public class CreateNewUserController {
+    @FXML
+    private TextField createUsernameField;
+    @FXML
+    private PasswordField createPasswordField;
+
+    @FXML
+    public static void main(String[] args) {
+
+
+    }
+    @FXML
+    public void onCreateUserButton(ActionEvent actionEvent) {
+
+
+    }
+    @FXML
+    public void onBackButton(ActionEvent actionEvent) {
+
+
+    }
+}
--- Java_Projects/GuestRegistration/GuestRegistration/src/main/java/com/royalhotel/guestregistration/GuestRegistrationController.java
+++ Java_Projects/GuestRegistration/GuestRegistration/src/main/java/com/royalhotel/guestregistration/GuestRegistrationController.java
@@ -2,9 +2,17 @@ package com.royalhotel.guestregistration;
 
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
 import javafx.scene.control.Label;
 import javafx.scene.control.PasswordField;
 import javafx.scene.control.TextField;
+import javafx.scene.layout.Pane;
+import javafx.stage.Stage;
+
+import java.io.IOException;
+import java.net.URL;
+import java.util.Objects;
 
 public class GuestRegistrationController {
     @FXML
@@ -26,9 +34,18 @@ public class GuestRegistrationController {
 
     }
     @FXML
-    public void onNewUserButtonClick(ActionEvent actionEvent) {
-        String username = usernameTextField.getText();
-        String password = passwordPasswordField.getText();
+    public void onNewUserButtonClick(ActionEvent actionEvent){
+        try {
+            Stage stage = new Stage();
+            FXMLLoader fxmlLoader = new FXMLLoader();
+            Pane root = fxmlLoader.load(Objects.requireNonNull(getClass().getResource("CreateNewUser.fxml")).openStream());
+            stage.setScene(new Scene(root,700,488));
+            stage.setResizable(false);
+            stage.setTitle("Registration");
+            stage.showAndWait();
+        } catch (IOException e){
+            e.printStackTrace();
+        }
     }
 
 }
