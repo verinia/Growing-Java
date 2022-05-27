b21867973145b17515456e070aab5f734289a8e9
cjw0621 <77221921+cjw0621@users.noreply.github.com>
2022-05-26 23:33:28 -0400

    updated application

--- Java_Projects/GuestRegistration/GuestRegistration/src/main/java/com/royalhotel/guestregistration/GuestRegistrationController.java
+++ Java_Projects/GuestRegistration/GuestRegistration/src/main/java/com/royalhotel/guestregistration/GuestRegistrationController.java
@@ -1,11 +1,10 @@
 package com.royalhotel.guestregistration;
 
 import javafx.event.ActionEvent;
-import javafx.fxml.FXML;;
+import javafx.fxml.FXML;
 import javafx.scene.control.Label;
 import javafx.scene.control.PasswordField;
 import javafx.scene.control.TextField;
-;
 
 public class GuestRegistrationController {
     @FXML
